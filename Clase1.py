75ee21af02202501b21fa8e908d2048a9c98b788
DANIELP <DANIELP@JARANDES.COM>
2024-10-12 09:06:16 -0500

    first commit

---
+++ Clase1.py
@@ -0,0 +1,45 @@
+"""Actividad 3"""
+
+# print ("Hello World")
+
+"""Actividad 4"""
+
+# edad=19
+# altura=1.7
+# nombre="Daniel"
+# mascota= True
+# carros =["BMW","Ford","KIA"]
+
+# print (f'Nombre: {nombre}\nEdad: {edad} anos\nAltura: {altura} \nMascota: {mascota}\nCarros: {carros}')
+
+"""Actividad 5"""
+
+# Edad = 5
+
+# if (Edad > 18):
+#     print ("Eres mayor de edad")
+# else:
+#     print ("Eres menor de edad")
+
+"""Actividad 6"""
+
+
+# def Resultado (Edad):
+
+#     if (Edad >= 18):
+#         return True
+#     else:
+#         return False
+
+# Edad = 18
+
+# print("Edad del usuario ",Edad)
+# Final = Resultado (Edad)
+# print (Final)
+
+Num = int(input("Digita tu numero"))
+
+if Num % 2 == 0:
+    print("El Numero es par")
+else:
+    print("El numero es impar")
